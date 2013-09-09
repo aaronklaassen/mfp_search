271a1ee7baac5e674bb1af1a2b5a0dfa12e4cc7c
Aaron Klaassen <aaron@outerspacehero.com>
2013-09-09 14:04:01 -0500

    Show user-user-submitted on all the search results pages.

--- filter.js
+++ filter.js
@@ -1,12 +1,10 @@
 $(function() {
-  
+
   var url = chrome.extension.getURL("controls.html");
-  $(".col-1 .col-3").append($('<div>').load(url, function() {
+  $(".col-1 .col-3, .col-1 .col-2").append($('<div>').load(url, function() {
     $("a.hide-user-sub").click(function(e) {
       e.preventDefault();
       alert("clicky");
     });
   }));
- 
-  
 });
