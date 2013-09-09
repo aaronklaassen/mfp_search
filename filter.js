979d2b2f967631122ee30100c9020caa33d22666
Aaron Klaassen <aaron@outerspacehero.com>
2013-09-09 13:18:59 -0500

    Insert the hide-user-submitted control.

--- filter.js
+++ filter.js
@@ -1,3 +1,12 @@
 $(function() {
-  alert("Hello, world!");
+  
+  var url = chrome.extension.getURL("controls.html");
+  $(".col-1 .col-3").append($('<div>').load(url, function() {
+    $("a.hide-user-sub").click(function(e) {
+      e.preventDefault();
+      alert("clicky");
+    });
+  }));
+ 
+  
 });
