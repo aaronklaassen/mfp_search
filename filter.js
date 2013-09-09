9f7bb3ba18187425bdd27350f96bd05714751589
Aaron Klaassen <aaron@outerspacehero.com>
2013-09-09 14:15:16 -0500

    Working hide-user-submitted and add show.

--- filter.js
+++ filter.js
@@ -2,9 +2,29 @@ $(function() {
 
   var url = chrome.extension.getURL("controls.html");
   $(".col-1 .col-3, .col-1 .col-2").append($('<div>').load(url, function() {
+
     $("a.hide-user-sub").click(function(e) {
       e.preventDefault();
-      alert("clicky");
+      toggleUserSubmittedResults();
     });
+
+    $("a.show-user-sub").click(function(e) {
+      e.preventDefault();
+      toggleUserSubmittedResults();
+    });
+
   }));
 });
+
+function toggleUserSubmittedResults()
+{
+  $("ul#matching li a").each(function() {
+    if ($(this).html().charAt(0) == "*")
+    {
+      $(this).parent().toggle();
+      $("a.hide-user-sub").toggle();
+      $("a.show-user-sub").toggle();
+    }
+  });
+}
+
