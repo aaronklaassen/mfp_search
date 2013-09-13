63ac7af32c67b96292e2a8b2b44980a926988c95
Aaron Klaassen <aaron@outerspacehero.com>
2013-09-13 13:44:59 -0500

    Refactor show/hide user-subs. Add search-in-results filter.

--- filter.js
+++ filter.js
@@ -3,27 +3,50 @@ $(function() {
   var url = chrome.extension.getURL("controls.html");
   $(".col-1 .col-3, .col-1 .col-2").append($('<div>').load(url, function() {
 
-    $("a.hide-user-sub").click(function(e) {
+    $("a.show-hide-user-sub").click(function(e) {
       e.preventDefault();
       toggleUserSubmittedResults();
     });
 
-    $("a.show-user-sub").click(function(e) {
-      e.preventDefault();
-      toggleUserSubmittedResults();
+    $("#filter-by-text").keyup(function() {
+      hideNonMatchingResults($(this).val().toLowerCase().trim())
     });
 
   }));
 });
 
+
+
+
+
+function hideNonMatchingResults(needle)
+{
+  if (needle == "")
+  {
+    $("ul#matching li").each(function() {
+      $(this).show();
+    });
+  } else {
+
+    $("ul#matching li a").each(function() {
+      if ($(this).html().toLowerCase().indexOf(needle) == -1)
+      {
+        $(this).parent().hide();
+      } else {
+        $(this).parent().show();
+      }
+    });
+  }
+}
+
+
 function toggleUserSubmittedResults()
 {
+  $("a.show-hide-user-sub").toggle();
   $("ul#matching li a").each(function() {
     if ($(this).html().charAt(0) == "*")
     {
       $(this).parent().toggle();
-      $("a.hide-user-sub").toggle();
-      $("a.show-user-sub").toggle();
     }
   });
 }
